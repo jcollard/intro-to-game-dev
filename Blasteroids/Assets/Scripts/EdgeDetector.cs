c5f54d9b2acdc918f3b2a21e7c6012599eda4566
Joseph Collard <jcollard@chadwickschool.org>
2020-12-04 10:50:42 -0800

    Finished Activity 2

---
+++ Blasteroids/Assets/Scripts/EdgeDetector.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EdgeDetector : MonoBehaviour
+{
+
+    public float teleportY = 5.25f;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        print("Colliding with Edge!");
+
+        // Create a variable containing the object that collided with the edge
+        GameObject collidingObject = collision.gameObject;
+
+        print(collidingObject);
+
+        float currentX = collidingObject.transform.position.x;
+        collidingObject.transform.position = new Vector2(currentX, teleportY);
+
+    }
+
+}
---
+++ Blasteroids/Assets/Scripts/HorizontalEdgeDetector.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HorizontalEdgeDetector : MonoBehaviour
+{
+    public float teleportX = 5.25f;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        print("Colliding with Edge!");
+
+        // Create a variable containing the object that collided with the edge
+        GameObject collidingObject = collision.gameObject;
+
+        print(collidingObject);
+
+        float currentY = collidingObject.transform.position.y;
+        collidingObject.transform.position = new Vector2(teleportX, currentY);
+
+    }
+}
