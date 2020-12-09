37c5067f26b78388b17c3aa2abbd8498ad401fcc
Joseph Collard <jcollard@chadwickschool.org>
2020-12-09 14:49:32 -0800

    Added a LaserController and laser to the scene.

---
+++ Blasteroids/Assets/Scripts/LaserController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserController : MonoBehaviour
+{
+    // The speed the laser moves
+    public float speed = 3;
+
+    // The range the laser can travel
+    public float range = 15;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.up * speed * Time.deltaTime);
+
+        this.range = this.range - speed * Time.deltaTime;
+
+        if (this.range < 0)
+        {
+            print("Range is less than 0");
+            UnityEngine.Object.Destroy(this.gameObject);
+        }
+
+    }
+}
