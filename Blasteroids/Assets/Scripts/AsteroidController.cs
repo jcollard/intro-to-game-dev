b86d6765bb0de04419417665a9c78afea2ed7f05
Joseph Collard <jcollard@chadwickschool.org>
2020-12-11 06:05:50 -0800

    Added basic asteroids.

---
+++ Blasteroids/Assets/Scripts/AsteroidController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidController : MonoBehaviour
+{
+    // The initial force vector to apply to this 
+    public Vector2 initialForce;
+    public float rotationSpeed;
+    public Rigidbody2D rigidBody;
+
+    void Start()
+    {
+        rigidBody.AddForce(initialForce);
+    }
+
+    private void Update()
+    {
+        transform.Rotate(transform.forward * rotationSpeed * Time.deltaTime);
+    }
+
+}
